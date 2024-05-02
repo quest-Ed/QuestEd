3614f5001fa8e82cff898ea9fa2ccc5c25bb2d2c
Ankit <ankitbhandariiitr@gmail.com>
2024-05-01 21:26:18 -0400

    removed useless comments

--- server/app.js
+++ server/app.js
@@ -1,26 +1,3 @@
-// const express = require('express');
-// const app= express();
-// const expressWs = require('express-ws')(app);
-// const chatRouter = require('./routes/chat');
-
-
-// app.use(cors({
-//   origin: 'http://localhost:3001' // Allow your React app to connect
-// }));
-
-// const port = 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('Welcome to QuestEd!');
-// });
-
-// app.use('/chat', chatRouter);
-
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
